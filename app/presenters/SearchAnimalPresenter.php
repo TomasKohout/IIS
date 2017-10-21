8165d616fb0853fd4bda0ba14ffad93e62c972ec
Tomáš Blažek <tblazek33@gmail.com>
2017-10-21 20:48:00 +0200

    SearchAnimal

---
+++ app/presenters/SearchAnimalPresenter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomas
+ * Date: 21.10.17
+ * Time: 20:19
+ */
+
+namespace App\Presenters;
+use Nette;
+
+class SearchAnimalPresenter extends \Nette\Application\UI\Presenter
+{
+    public function renderDefault()
+    {
+
+    }
+
+}
