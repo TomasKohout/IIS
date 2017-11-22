2309ad95f7c9d031a1057ca4f85e754f162a7af0
Tomáš Blažek <tblazek33@gmail.com>
2017-11-22 01:40:43 +0100

    Merge conflict fixes

--- app/model/AuthorizationFactory.php
+++ app/model/AuthorizationFactory.php
@@ -34,10 +34,11 @@ class AuthorizationFactory
         $acl->addResource('admin');
         $acl->addResource('addKind');
 
-        $acl->allow('2', ['animal', 'training', 'coop'], 'view');
+        $acl->allow('2', ['animal', 'training', 'coop', 'clean', 'feed'], 'view');
         $acl->allow('2', ['clean', 'feed'], 'add');
 
         $acl->allow('1' ,['animal'], 'add');
+        $acl->allow('1' ,['animal'], 'view');
 
         $acl->allow('0', 'admin');
         $acl->allow('0', ['coop','addKind', 'training'], 'add');
